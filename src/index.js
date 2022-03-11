c46ddc0e6ed07d3e1edb98f03ba9808860ea2829
rukundo725 <gerukundo14@gmail.com>
2022-03-11 06:44:47 +0200

    merged the dbConnection to createArticle

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-// import routes from './Routers/index'
+import mongoose from 'mongoose';
+import connectDb from './dbConn';
+import blogRoute from "./Routers/blog";
+
+
+
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// middleware 
+app.use(express.json());
 app.use(bodyParser.json());
+app.use('/api/blog', blogRoute);
 
-const port = 7000;
+const port = 4000;
 
-app.listen(port, ()=>{
-    console.log('listening on port '+port);
-})
-
-// app.use('/', routes);
-
-
-export default app;
+// Connect to MongoDB
+connectDb();
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB');
+    app.listen(process.env.port || port, () => console.log(`Server running on port ${port}`));
+});
