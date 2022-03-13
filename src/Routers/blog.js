bb2d8e2c3e87bc01ba50faca43f95bd1bff90831
rukundo725 <gerukundo14@gmail.com>
2022-03-13 22:27:10 +0200

    added an end point for getting all  Articles

--- src/Routers/blog.js
+++ src/Routers/blog.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createArticle, getOneArticle, deleteArticle, updateArticle,} from "../Controllers/blog";
+import { createArticle, getOneArticle, deleteArticle, updateArticle, getAllArticle} from "../Controllers/blog";
 
 
 const router = express.Router();
@@ -8,6 +8,7 @@ router.post("/add", createArticle);
 router.get("/:id", getOneArticle);
 router.delete("/:id", deleteArticle);
 router.put("/:id", updateArticle);
+router.get("/", getAllArticle);
 
 
 
