7a32f2948caa9c1b50efb6991910dde3a9576702
rukundo725 <gerukundo14@gmail.com>
2022-03-19 09:48:28 +0200

    updated the test file

--- tests/appTest.js
+++ tests/appTest.js
@@ -2,6 +2,7 @@ import chai from "chai";
 import chaiHttp from "chai-http";
 import app from "../src/index";
 
+
 chai.should();
 chai.use(chaiHttp);
 
@@ -20,8 +21,8 @@ describe("Blog API", ()=>{
                     response.body.should.be.a('array');
                  
                 done();
-                })
-        })
+                });
+        });
 
         // in case we pass the wrong url 
 
@@ -266,4 +267,84 @@ describe("Blog API", ()=>{
 
 
 })
+
+// ********************************************************************************************* 
+
+
+
+describe('Login Routes TEST', () => {
+    // should not post a user due to wrong info
+        describe("POST api/user/register", () =>{
+            it("It should not create a new user due to a not hashed password", (done) =>{
+                const CreateUser = {
+                    username: process.env.username,
+                    email: process.env.email,
+                    password: process.env.password
+                };
+                chai.request(app)
+                .post("/api/user/register")
+                .send(CreateUser)
+                .end((err, response) => {
+                    response.should.have.status(500);
+                done();
+        
+                });
+            });
+
+            // in case we pass a wrong url 
+
+            it("It should not create a user due to wrong url", (done)=>{
+                const CreateUser = {
+                    username: process.env.username,
+                    email: process.env.email,
+                    password: process.env.password
+                };
+                chai.request(app)
+                    .put("/api/user/reg")
+                    .send(CreateUser)
+                    .end((err, response)=> {
+                        response.should.have.status(404); 
+                    done();
+                    })
+            })
+
+        });
+    
+        // log in
+        describe("POST api/user/login", () =>{
+            it("should login", (done) =>{
+                const LoginInfo = {
+                    username: process.env.username,
+                    email: process.env.email,
+                    password: process.env.password
+                };
+                chai.request(app)
+                .post("/api/user/login")
+                .send(LoginInfo)
+                .end((err, response) => {
+                    response.should.have.status(200);
+                done();
+        
+                });
+            });
+
+            // in case we pass a wrong url 
+
+            it("It should not let  a user to log in due to wrong url", (done)=>{
+                const LoginInfo = {
+                    username: process.env.username,
+                    email: process.env.email,
+                    password: process.env.password
+                };
+                chai.request(app)
+                    .put("/api/user/log")
+                    .send(LoginInfo)
+                    .end((err, response)=> {
+                        response.should.have.status(404); 
+                    done();
+                    })
+            })
+        });
+         
+    })
 
