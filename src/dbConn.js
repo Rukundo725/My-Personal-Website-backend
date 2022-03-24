4422161d90dbd1e5c08cdc4e7785b7c4f552c228
rukundo725 <gerukundo14@gmail.com>
2022-03-24 23:26:59 +0200

    added a heroku ci/cd

--- src/dbConn.js
+++ src/dbConn.js
@@ -4,7 +4,7 @@ import express from 'express'
 // connect to mongodb & listen for requests
 const connectDb = async () =>{
     try{
-        mongoose.connect(process.env.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true }); await parseAndLoadPlanetsData();
+        mongoose.connect(process.env.dbUrl, { useNewUrlParser: true, useUnifiedTopology: true })
     }catch(err){
         console.error(err);
     }
