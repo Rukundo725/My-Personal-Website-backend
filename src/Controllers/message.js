f30ab3b400c02bc959a312b4568aa359a3842aaa
rukundo725 <gerukundo14@gmail.com>
2022-03-25 21:51:44 +0200

    created messages api controllers

---
+++ src/Controllers/message.js
@@ -0,0 +1,22 @@
+import Message from "../../models/comment";
+//send messages
+export const sendMessage = async (req, res) => {
+  try {
+    await Message.create(req.body);
+    res.status(201).json({ status: "success", code: "200",message: "Message sent" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ status: "error", code: "500",message: "Message not sent, internal_server_error" });
+
+  }
+};
+
+//get All Messages
+export const getMessages = async (req, res) => {
+  try {
+    const allMessages = await Message.find();
+    res.status(200).json({status: "success", code: "200", Message: allMessages});
+  } catch (error) {
+    res.status(500).json({ status: "error", code: "500",message: "Can not get messages, internal_server_error" });
+  }
+};
