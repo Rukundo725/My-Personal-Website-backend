1b3d4f8e17f49690b852d6edeb5fb1a8a834105f
rukundo725 <gerukundo14@gmail.com>
2022-03-25 22:07:51 +0200

    created comments api routes

---
+++ src/Routers/comment.js
@@ -0,0 +1,11 @@
+import express from "express";
+import {verifyToken} from "./Auth";
+import { sendComment, getComments} from "../Controllers/message";
+
+const router = express.Router();
+
+router.post("/comment", sendComment);
+router.get("/", verifyToken, getComments);
+
+export default router;
+
